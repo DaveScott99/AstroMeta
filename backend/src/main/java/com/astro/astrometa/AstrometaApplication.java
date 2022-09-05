2a75be343d7aaa136017edaba1928a5448366744
DaviSantos <davicalixto2077@gmail.com>
2022-09-05 17:18:46 -0300

    project-created

---
+++ backend/src/main/java/com/astro/astrometa/AstrometaApplication.java
@@ -0,0 +1,13 @@
+package com.astro.astrometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrometaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/astro/astrometa/AstrometaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.astrometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
