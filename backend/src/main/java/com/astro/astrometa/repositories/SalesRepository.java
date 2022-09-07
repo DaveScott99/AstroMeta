a17ea757138f8380a973b7010d2f1003ac79bb92
DaviSantos <davicalixto2077@gmail.com>
2022-09-07 17:38:54 -0300

    Date select

--- backend/src/main/java/com/astro/astrometa/repositories/SalesRepository.java
+++ backend/src/main/java/com/astro/astrometa/repositories/SalesRepository.java
@@ -1,9 +1,18 @@
 package com.astro.astrometa.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.astro.astrometa.entities.Sale;
 
 public interface SalesRepository extends JpaRepository<Sale, Long> {
 
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+	
 }
