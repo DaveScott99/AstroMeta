68aaba816f1c3caf6836d86e5593cd7301c78bb2
DaviSantos <davicalixto2077@gmail.com>
2022-09-10 18:56:44 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from "../NotificationButton";
 import "./styles.css";
 
@@ -12,10 +14,12 @@ function SalesCard() {
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
 
+  const [sales, setSales] = useState<Sale[]>([]);
+
   useEffect(() => {
-    axios.get("http://localhost:8080/sales").then(response => {
-      console.log(response.data)
-    })
+    axios.get(`${BASE_URL}/sales`).then((response) => {
+      setSales(response.data.content);
+    });
   }, []);
 
   return (
@@ -56,96 +60,25 @@ function SalesCard() {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="show992">#268</td>
-                <td className="show576">25/08/2022</td>
-                <td>Anakin</td>
-                <td className="show992">20</td>
-                <td className="show992">14</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="astrometa-red-btn-container">
-                    <div className="astrometa-red-btn">
-                      <NotificationButton />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#268</td>
-                <td className="show576">25/08/2022</td>
-                <td>Anakin</td>
-                <td className="show992">20</td>
-                <td className="show992">14</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="astrometa-red-btn-container">
-                    <div className="astrometa-red-btn">
-                      <NotificationButton />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#268</td>
-                <td className="show576">25/08/2022</td>
-                <td>Anakin</td>
-                <td className="show992">20</td>
-                <td className="show992">14</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="astrometa-red-btn-container">
-                    <div className="astrometa-red-btn">
-                      <NotificationButton />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#268</td>
-                <td className="show576">25/08/2022</td>
-                <td>Anakin</td>
-                <td className="show992">20</td>
-                <td className="show992">14</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="astrometa-red-btn-container">
-                    <div className="astrometa-red-btn">
-                      <NotificationButton />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#268</td>
-                <td className="show576">25/08/2022</td>
-                <td>Anakin</td>
-                <td className="show992">20</td>
-                <td className="show992">14</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="astrometa-red-btn-container">
-                    <div className="astrometa-red-btn">
-                      <NotificationButton />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#268</td>
-                <td className="show576">25/08/2022</td>
-                <td>Anakin</td>
-                <td className="show992">20</td>
-                <td className="show992">14</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="astrometa-red-btn-container">
-                    <div className="astrometa-red-btn">
-                      <NotificationButton />
-                    </div>
-                  </div>
-                </td>
-              </tr>
+              {sales.map((sale) => {
+                return (
+                  <tr key={sale.id}>
+                    <td className="show992">{sale.id}</td>
+                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                    <td>{sale.sellerName}</td>
+                    <td className="show992">{sale.visited}</td>
+                    <td className="show992">{sale.deals}</td>
+                    <td>R$ {sale.amount.toFixed(2)}</td>
+                    <td>
+                      <div className="astrometa-red-btn-container">
+                        <div className="astrometa-red-btn">
+                          <NotificationButton />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
