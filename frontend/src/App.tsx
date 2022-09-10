b14ce7e04d880faeed5d829974e3c0f42b6473d0
DaviSantos <davicalixto2077@gmail.com>
2022-09-10 19:25:06 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import SalesCard from "./components/SalesCard";
 import Header from "./components/Header";
+
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="astrometa-sales">
